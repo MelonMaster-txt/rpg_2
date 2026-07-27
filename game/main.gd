2d50bc29fcd4ab9f7412065caf0528e7d328e9f1
MelonMaster-txt <poubelleajeter05@gmail.com>
2026-07-27 10:23:11 +0200

    fix: instanciate HUD at startup in main.gd

--- game/main.gd
+++ game/main.gd
@@ -1,4 +1,14 @@
 extends Node
 
+const HUD_SCENE = preload("res://game/ui/hud.tscn")
+
+var _hud: CanvasLayer = null
+
 func _ready() -> void:
+	_spawn_hud()
 	SceneManager.load_start_scene()
+
+func _spawn_hud() -> void:
+	if _hud == null:
+		_hud = HUD_SCENE.instantiate()
+		add_child(_hud)
