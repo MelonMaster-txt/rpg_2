b61fe33747ea4efe85ab6cb1126798e780c43b93
MelonMaster-txt <poubelleajeter05@gmail.com>
2026-07-29 12:36:55 +0200

    fix: ChunkManager script dans tscn + appel manuel deferred_init + spawn player avant init chunks

--- game/world/overworld.gd
+++ game/world/overworld.gd
@@ -1,25 +1,23 @@
 extends Node2D
 
-const PLAYER_SCENE      := preload("res://game/characters/player/player.tscn")
-const FARM_PLACER_SCR   := preload("res://game/world/farm_placer.gd")
-const CHUNK_MANAGER_SCR := preload("res://game/world/chunk_manager.gd")
+const PLAYER_SCENE    := preload("res://game/characters/player/player.tscn")
+const FARM_PLACER_SCR := preload("res://game/world/farm_placer.gd")
 
 @onready var _player_container : Node2D   = $PlayerContainer
 @onready var _player_spawn     : Marker2D = $PlayerSpawn
 @onready var _chunk_manager    : Node2D   = $ChunkManager
 
 func _ready() -> void:
-	# Attache le script au noeud ChunkManager
-	if _chunk_manager.get_script() == null:
-		_chunk_manager.set_script(CHUNK_MANAGER_SCR)
-
-	# Farm placer (Node2D avec script)
+	# Farm placer
 	var fp := Node2D.new()
 	fp.set_script(FARM_PLACER_SCR)
 	fp.name = "FarmPlacer"
 	add_child(fp)
 
-	call_deferred("_spawn_player")
+	# Spawner le joueur d'abord, puis laisser ChunkManager le trouver
+	_spawn_player()
+	# _deferred_init du ChunkManager sera appele au prochain frame
+	# (call_deferred deja dans chunk_manager._ready())
 
 func _spawn_player() -> void:
 	var player : Node2D = PLAYER_SCENE.instantiate() as Node2D
