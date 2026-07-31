24b3ae882da62f8a7ed19e2b4050e7e44670384e
MelonMaster-txt <poubelleajeter05@gmail.com>
2026-07-31 17:24:50 +0200

    Some necessary changes

--- game/ui/hud.gd
+++ game/ui/hud.gd
@@ -3,6 +3,14 @@ extends CanvasLayer
 
 @onready var time_label: Label = $HUDPanel/HUDContainer/TimeLabel
 @onready var day_label:  Label = $HUDPanel/HUDContainer/DayLabel
+@onready var money_label:  Label = $HUDPanel/HUDContainer/MoneyLabel
+@onready var life_label:  Label = $HUDPanel/HUDContainer/LifeLabel
+@onready var Force_label:  Label = $HUDPanel/HUDContainer/ForceLabel
+@onready var Stamina_label:  Label = $HUDPanel/HUDContainer/StaminaLabel
+@onready var Luck_label:  Label = $HUDPanel/HUDContainer/LuckLabel
+@onready var Intelligence_label:  Label = $HUDPanel/HUDContainer/IntelligenceLabel
+@onready var Charisma_label:  Label = $HUDPanel/HUDContainer/CharismaLabel
+
 @onready var inventory_screen = $InventoryScreen
 
 func _ready() -> void:
@@ -19,6 +27,13 @@ func _unhandled_key_input(event: InputEvent) -> void:
 func _refresh_all() -> void:
 	time_label.text = "Heure: " + GameManager.get_time_string()
 	day_label.text  = "Jour %d" % GameManager.current_day
+	money_label.text = "Argent: %d" % GameManager.get_item("money")
+	life_label.text = "Vie: %d" % GameManager.life
+	Force_label.text = "Force: %d" % GameManager.force
+	Stamina_label.text = "Endurance: %d" % GameManager.stamina
+	Luck_label.text = "Chance: %d" % GameManager.luck
+	Intelligence_label.text = "Intelligence: %d" % GameManager.intelligence
+	Charisma_label.text = "Charisme: %d" % GameManager.charisma
 
 func _on_time_changed(h: int, m: int, d: int) -> void:
 	time_label.text = "%02d:%02d" % [h, m]
